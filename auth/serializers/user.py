ea0c7c0784bc02f7c2166439664eaf76cd3072cd
Gustavo Santos <alexandregustavo00@gmail.com>
2025-02-11 20:41:27 -0300

    feat: implementa os serializadores de user e autenticaçao

---
+++ auth/serializers/user.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields=('id','name','cpf','email','role','is_student','is_teacher','is_manager','created_at','updated_at')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+    
+    def create(self, validated_data):
+        role = validated_data.pop('role')
+        user = User.objects.create_user_entity(role, **validated_data)
+        return user 
+
+class LoginSerializer(serializers.Serializer):
+    cpf = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(cpf=attrs['cpf'], password=attrs['password'])
+        if not user:
+            raise serializers.ValidationError('Incorrect credentials')
+        return {'user': user}
+
+    
