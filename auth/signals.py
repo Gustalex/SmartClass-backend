7451919ad5b90445c534ff2c057a5fdc205e5e68
Gustavo Santos <alexandregustavo00@gmail.com>
2025-02-19 10:54:35 -0300

    feat: implemneta a criaçao de um usuario root manager automaticamente

---
+++ auth/signals.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+User = get_user_model()
+
+receiver(post_migrate)
+def create_root_user(sender, **kwargs):
+    if sender.name == 'auth':
+        user_name = settings.ROOT_NAME
+        cpf = settings.ROOT_CPF
+        email = settings.ROOT_EMAIL
+        password = settings.ROOT_PASSWORD
+        role = settings.ROOT_ROLE
+    
+        if not User.objects.filter(cpf=cpf).exists():
+            User.objects.create_user_entity(role, cpf, email, password, name=user_name)
+        
--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,3 +152,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Root manager user credentials
+
+ROOT_NAME = config('ROOT_NAME')
+ROOT_CPF = config('ROOT_CPF')
+ROOT_EMAIL = config('ROOT_EMAIL')
+ROOT_PASSWORD = config('ROOT_PASSWORD')
+ROOT_ROLE = config('ROOT_ROLE')
