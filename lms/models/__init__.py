919c82a3f7a5d777502ba463b04b5a7e6093e046
Gustavo Santos <alexandregustavo00@gmail.com>
2025-03-05 16:21:05 -0300

    feat: implementa o modelo de turma

--- lms/models/__init__.py
+++ lms/models/__init__.py
@@ -3,3 +3,4 @@ from .materia import *
 from .factory_model import *
 from .aula import *
 from .atividade import *
+from .turma import *
---
+++ lms/models/turma.py
@@ -0,0 +1,17 @@
+from django.db import models
+from .factory_model import FactoryModel
+
+class Turma(FactoryModel):
+    nome = models.CharField(max_length=255)
+    curso = models.ForeignKey('Curso', on_delete=models.CASCADE, related_name='turmas_curso')  
+    materia = models.ForeignKey('Materia', on_delete=models.CASCADE, related_name='turmas_materia')
+    professor = models.ForeignKey('auth_service.User', on_delete=models.DO_NOTHING, related_name='turmas_professor')
+    alunos = models.ManyToManyField('auth_service.User', related_name='turmas_aluno')  
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        db_table = 'turma'
+        verbose_name = 'Turma'
+        verbose_name_plural = 'Turmas'
