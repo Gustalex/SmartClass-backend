720030de919b4ec6bce39d24d7f44f69c686f061
Gustavo Santos <alexandregustavo00@gmail.com>
2025-03-07 00:18:02 -0300

    fix: ajusta o serializador de usuario para tratar o update e corrige um erro na configuracao do cors

--- auth_service/serializers/user.py
+++ auth_service/serializers/user.py
@@ -19,17 +19,18 @@ class UserSerializer(serializers.ModelSerializer):
         curso = data.get('curso')
         cursos = data.get('cursos')
 
-        if role == 'student' or role == None and not curso:
-            raise serializers.ValidationError("O campo 'curso' é obrigatório para alunos.")
+        if 'role' in data:
+            if (role == 'student' or role is None) and not curso:
+                raise serializers.ValidationError("O campo 'curso' é obrigatório para alunos.")
 
-        if role == 'teacher' and not cursos:
-            raise serializers.ValidationError("O campo 'cursos' é obrigatório para professores.")
+            if role == 'teacher' and not cursos:
+                raise serializers.ValidationError("O campo 'cursos' é obrigatório para professores.")
 
-        if role == 'student' and cursos:
-            raise serializers.ValidationError("Alunos não podem estar associados a múltiplos cursos. Use o campo 'curso'.")
+            if role == 'student' and cursos:
+                raise serializers.ValidationError("Alunos não podem estar associados a múltiplos cursos. Use o campo 'curso'.")
 
-        if role == 'teacher' and curso:
-            raise serializers.ValidationError("Use o campo 'cursos'.")
+            if role == 'teacher' and curso:
+                raise serializers.ValidationError("Use o campo 'cursos'.")
 
         return data
 
@@ -47,6 +48,33 @@ class UserSerializer(serializers.ModelSerializer):
             **validated_data
         )
         return user
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.cpf = validated_data.get('cpf', instance.cpf)
+
+        role = validated_data.get('role')
+        if role is not None:
+            instance.is_student = False
+            instance.is_teacher = False
+            instance.is_manager = False
+
+            if role == 'student' or role is None:
+                instance.is_student = True
+                instance.curso = validated_data.get('curso', instance.curso)
+                instance.cursos.clear()
+            elif role == 'teacher':
+                instance.is_teacher = True
+                instance.curso = None  
+                instance.cursos.set(validated_data.get('cursos', instance.cursos.all()))
+            elif role == 'manager':
+                instance.is_manager = True
+                instance.curso = None  
+                instance.cursos.clear()  
+
+        instance.save()
+        return instance
     
 class LoginSerializer(serializers.Serializer):
     cpf = serializers.CharField()
